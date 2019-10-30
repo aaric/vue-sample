ffc0fe101485a0df8b64fb84e19e3f65c006f812
aaric <vipaaric@gmail.com>
2019-10-30 11:49:13 +0800

    v3.0 Keep Improve

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
-//App.vue
 <template>
   <div id="app">
-    <Navbar />
-    <HelloWorld/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Navbar from './components/Navbar.vue';
+
 export default {
-  name: 'navbar',
+  name: 'app',
   components: {
-    Navbar,
     HelloWorld
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
