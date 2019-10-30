d2f30863fc2f24f2b72e50f0e55c9a862c8ee279
aaric <vipaaric@gmail.com>
2019-10-30 17:28:56 +0800

    v3.1 Fetch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <usage-fetch />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import UsageFetch from './components/UsageFetch.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    //HelloWorld
+    UsageFetch
   }
 }
 </script>
