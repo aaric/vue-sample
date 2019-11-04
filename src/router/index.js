7b9eef06835840e72bafc4b579f1bc1369807485
aaric <vipaaric@gmail.com>
2019-11-04 10:05:56 +0800

    v3.7 vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '@/components/Home.vue'
-import HomeAbout from '@/components/HomeAbout.vue'
+import Payment from '@/components/Payment.vue'
 
 Vue.use(VueRouter)
 
@@ -10,7 +10,7 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/home', component: Home },
-    { path: '/home/about', component: HomeAbout },
+    { path: '/payment', component: Payment },
     { path: '/', redirect: Home }
   ]
 })
